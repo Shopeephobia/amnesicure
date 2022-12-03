f20d2a7cd00199bdbc8b81e7385c86fee400058c
stndaru <stefanus.ndaru@ui.ac.id>
2022-12-03 08:27:14 +0700

    Fix url for deck again

--- deck/urls.py
+++ deck/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from deck.views import deck_index, create_deck, fetch_public_deck, fetch_requests, create_request, verify_request, reject_request
+from deck.views import deck_index, create_deck, fetch_public_deck, fetch_requests, fetch_request_private, create_request, verify_request, reject_request
 
 app_name = "deck"
 
 urlpatterns = [
     path('', deck_index, name='index'),
     path('new', create_deck, name='create_deck'),
-    path('public', fetch_public_deck, name='create_deck'),
-    path('requests', create_request, name='create_request'),
+    path('public', fetch_public_deck, name='fetch_public_deck'),
+    path('requests', fetch_request_private, name='fetch_request_private'),
     path('requests/new/<int:pk>', create_request, name='create_request'),
     path('admin/requests', fetch_requests, name='fetch_requests'),
     path('admin/requests/verify/<int:pk>', verify_request, name='verify_deck'),
