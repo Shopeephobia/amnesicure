aacc19da382f44c22d73b71d03f37f18b5e79bdc
Muhammad Athallah <28977286+determinedguy@users.noreply.github.com>
2023-09-07 12:52:31 +0700

    update: allow any host

--- amnesicure/settings.py
+++ amnesicure/settings.py
@@ -38,7 +38,7 @@ DEBUG = True
 
 APP_NAME = os.getenv('APP_NAME', '')
 
-ALLOWED_HOSTS = [f'{APP_NAME}.up.railway.app']
+ALLOWED_HOSTS = ['*']
 
 if not PRODUCTION:
     ALLOWED_HOSTS += ['.localhost', '127.0.0.1', '[::1]']
